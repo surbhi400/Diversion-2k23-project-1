0b54752ede6a6ac83ec1bf6a3a197d2467e157b7
Shiwangi Kumari <sshiwangi770@gmail.com>
2023-02-22 12:26:26 +0530

    connecting to mongodb atlas

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,3 +1,18 @@
+// const mongoose = require("mongoose");
+// mongoose.set("strictQuery", false);
+
+// const DB = 'mongodb+srv://shiwangi14:Shiwangi@542800@cluster0.3awfjpk.mongodb.net/Theunirentals?retryWrites=true&w=majority'
+// mongoose.connect("DB", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true, 
+// })
+// .then(() => {
+//     console.log(`connection successful`);
+// }).catch((err)=>{
+//     console.log(err);
+// });
+
+
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 mongoose.connect("mongodb://0.0.0.0:27017/diversion", {
