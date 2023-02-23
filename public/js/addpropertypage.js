dccaf44fcd44164c5b8907fc97a4cbe672b456f2
Shiwangi Kumari <sshiwangi770@gmail.com>
2023-02-24 02:30:13 +0530

    updating index file

--- public/js/addpropertypage.js
+++ public/js/addpropertypage.js
@@ -4,18 +4,20 @@ submit_btn.addEventListener("click", function(e) {
     e.preventDefault();
 })
 // const items_container_pg = document.querySelector(".items-container-hostels");
-const location_container = document.querySelector('#location');
-const entered_location = location_container.value;
+// const location_container = document.querySelector('#location');
+// const entered_location = location_container.value;
 
-const selected_propertytype_value = document.querySelector('#propertytype').value;
-fetch(`/addproperty.hbs/:${entered_location}/:${selected_propertytype_value}`).then(res => {
-    return res.json();
-}).then((data) => {
-    data.forEach(element => {
-        console.log(element);
-    });
-}).catch((error) => {
-    console.log(error);
-    console.log("couldn't fetch data");
-})
+// const selected_propertytype_value = document.querySelector('#propertytype').value;
+// console.log(entered_location);
+// console.log(selected_propertytype_value);
+// fetch(`/addproperty.hbs/:${entered_location}/:${selected_propertytype_value}`).then(res => {
+//     return res.json();
+// }).then((data) => {
+//     data.forEach(element => {
+//         console.log(element);
+//     });
+// }).catch((error) => {
+//     console.log(error);
+//     console.log("couldn't fetch data");
+// })
 
